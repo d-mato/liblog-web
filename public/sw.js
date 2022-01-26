6969fb95900a8466a540412a21d578b070409ef0
Daiki Matoba <telnetstat@gmail.com>
2022-01-26 21:52:10 +0900

    Handle FCM notification

--- public/sw.js
+++ public/sw.js
@@ -9,3 +9,40 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   console.log(event)
 })
+
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js')
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDbnu85iKQ_ttu-Dlo8gg4g3lc1Z0xZpO0',
+  authDomain: 'liblog-82d18.firebaseapp.com',
+  projectId: 'liblog-82d18',
+  storageBucket: 'liblog-82d18.appspot.com',
+  messagingSenderId: '679472288411',
+  appId: '1:679472288411:web:ef86bcef1b4061d8bf2c3e'
+}
+
+firebase.initializeApp(firebaseConfig)
+const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log('onBackgroundMessage')
+  console.log(payload)
+
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body
+  })
+})
+
+messaging
+  .getToken({ vapidKey: 'BIG5JfJDrbiX68TWxDxyQlirVXBHzZUP-1-nP8Ebn5ncZykBtbbXWGhhuUPLk7cmpmAbI32UNrTInH1Jzobaob4' })
+  .then(currentToken => {
+    if (currentToken) {
+      console.log(currentToken)
+    } else {
+      console.error('No registration token available. Request permission to generate one.')
+    }
+  })
+  .catch(err => {
+    console.error('An error occurred while retrieving token. ', err)
+  })
--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,26 @@ export let name;
 export let messages = [];
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js').then(function (event) {
-    console.log(event)
+  navigator.serviceWorker.register('./sw.js').then(registration => {
+    console.log(registration)
     messages = [`registered`, ...messages]
+
+    registration.pushManager.getSubscription().then(subscription => {
+      if (subscription !== null) {
+        messages = ['subscribed', ...messages]
+      } else {
+        messages = ['NOT subscribed', ...messages]
+
+        const options = {
+          userVisibleOnly: true,
+        }
+        registration.pushManager.subscribe(options).then(subscription => {
+          messages = ['subscribed', ...messages]
+        }).catch(error => {
+          console.error(error)
+        })
+      }
+    })
   })
 }
 </script>
@@ -14,7 +31,7 @@ if ('serviceWorker' in navigator) {
 	<h1>Hello {name}!</h1>
   <div style="border: solid 1px #000; padding: 1rem;">
     {#each messages as message}
-      {message}
+      <div>{message}</div>
     {/each}
   </div>
 </main>
